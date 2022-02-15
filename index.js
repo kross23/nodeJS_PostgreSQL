45152f3b238ad21f803832005f3582fbd0829aab
Cross <662242@mail.ru>
2022-02-15 15:52:15 +0500

    morgan

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const models = require("./models/models");
 const router =  require('./routes/index');
 const req = require('express/lib/request');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const morgan = require('morgan');
 const path = require('path');
 require('dotenv').config();
 
@@ -13,8 +14,8 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(morgan('dev'));
 app.use(cors());
-
 app.use(express.json());
 app.use('/api', router);
 
